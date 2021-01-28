05f3511b4d08091c490ec74133000eab9110aaba
Roman Heinrich <roman.heinrich@gmail.com>
2021-01-28 16:45:12 +0100

    Import `vite/dynamic-import-polyfill` manually

--- demo/assets_new/src/main.tsx
+++ demo/assets_new/src/main.tsx
@@ -1,3 +1,6 @@
+// Needed, because automatic injection via HTML manipulation is not possible.
+import 'vite/dynamic-import-polyfill'
+
 // imported from phx
 import './vendor/phx-app.css'
 import "./vendor/phx-app.js"
